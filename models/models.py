19b83c6ee861414a5b8a7f35e8d79c8111b51944
dmithamo <dennisbmithamo@gmail.com>
2018-09-05 10:23:32 +0300

    [feature #160244252] : Build out the POST /orders endpoint

---
+++ models/models.py
@@ -0,0 +1,80 @@
+"""
+    Model an Order, ShoppingCart
+    Define method for creating an order.
+"""
+from datetime import datetime
+
+
+class Order:
+    """
+        Define Order params
+    """
+
+    def __init__(self, item_name, item_price, item_id=0, item_quantity=1, item_ordered_on=''):
+        """
+            Initialize an order object
+        """
+        self.item_id = item_id
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+        self.item_ordered_on = item_ordered_on
+
+    def __repr__(self):
+        """
+            Define how Order is represented in Terminal
+        """
+        return '{} - {} : {}'.format(
+            self.item_id, self.item_name, self.item_price)
+
+
+class ShoppingCart:
+    """
+        Define ShoppingCart methods
+    """
+
+    def __init__(self):
+        """
+            Initialize ShoppingCart object
+        """
+        self.cart = []
+
+    def __repr__(self):
+        """
+            Define a means or representing ShoppingCart in Terminal
+        """
+        cart_list = [{item.item_name : item.item_price} for item in self.cart]
+        return '{}'.format(cart_list)
+
+    def place_order(self, order_params):
+        """
+            Render order_params as Order object and
+            add Order to cart
+        """
+        # Check whether item already in cart
+        # Add new if not,
+        # Increase quantity if yes
+
+        items_in_cart = [item.item_name for item in self.cart]
+
+        if order_params['item_name'] in items_in_cart:
+            # If ordered item is already in cart,
+            # Increase quantity
+            for item in self.cart:
+                if item.item_name == order_params['item_name']:
+                    order = item
+                    order.item_quantity += 1
+                    break
+        else:
+            # Create new order
+            order = Order(order_params['item_name'], order_params['item_price'])
+
+            # Assign an item_id to newly created order
+            # by making reference to the number of items in cart
+            order.item_id = len(self.cart) + 1
+            # Append timestamp to order
+            order.item_ordered_on = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            # Add to shopping cart
+            self.cart.append(order)
+        return order
