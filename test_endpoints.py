fc1a6b7c8093c099681cef8f3e45a998079e64d1
dmithamo <dennisbmithamo@gmail.com>
2018-09-06 11:47:26 +0300

    [chore #160244698] : Refactor POST /orders endpoint tests

--- test_endpoints.py
+++ test_endpoints.py
@@ -2,6 +2,7 @@
     Tests for the API's endpoints.
 """
 
+import json
 import pytest
 import config
 from api.api import API
@@ -19,7 +20,15 @@ ORDER_2 = {
     'price' : 'Ksh. 1080'
 }
 
-@pytest.fixture
+def response_as_json(resp):
+    """
+        Helper function
+        Loads response as json for easier inspection
+    """
+    resp_json = json.loads(resp.data.decode('utf-8'))
+    return resp_json
+
+@pytest.fixture(scope='function')
 def api_test_client():
     """
         Initialize a standard testing fixture on which all tests
@@ -39,11 +48,36 @@ def test_post_endpoint(api_test_client):
         BASE_URL, ORDER['name'], ORDER['price']
     ))
     assert response.status_code == 201
+    assert 'Big Samosa' in str(response.data)
     assert 'ordered_on' in str(response.data)
 
 def test_post_endpoint_2(api_test_client):
     """
-        2. Test POST /orders - with some data missing
+        2. Test POST /orders - when similar order already exists
+    """
+    response = api_test_client.post('{}/orders?name={}&price={}'.format(
+        BASE_URL, ORDER['name'], ORDER['price']
+    ))
+    assert response.status_code == 201
+    assert 'Big Samosa' in str(response.data)
+    assert response_as_json(response)['quantity'] == 2
+    assert response_as_json(response)['item_id'] == 1
+
+def test_post_endpoint_3(api_test_client):
+    """
+        3. Test POST /orders - 2nd POST with proper data
+    """
+    response = api_test_client.post('{}/orders?name={}&price={}'.format(
+        BASE_URL, ORDER_2['name'], ORDER_2['price']
+    ))
+    assert response.status_code == 201
+    assert 'Pork Ribs' in str(response.data)
+    assert response_as_json(response)['item_price'] == 'Ksh. 1080'
+    assert response_as_json(response)['item_id'] == 2
+
+def test_post_endpoint_4(api_test_client):
+    """
+        4. Test POST /orders - with some data missing
     """
     response = api_test_client.post('{}/orders?name={}&price='.format(
         BASE_URL, ORDER['name']
@@ -51,9 +85,9 @@ def test_post_endpoint_2(api_test_client):
     assert response.status_code == 400
     assert 'Bad request' in str(response.data)
 
-def test_post_endpoint_3(api_test_client):
+def test_post_endpoint_5(api_test_client):
     """
-        3. Test POST /orders - without any data
+        5. Test POST /orders - without any data
     """
     response = api_test_client.post('{}/orders'.format(
         BASE_URL
