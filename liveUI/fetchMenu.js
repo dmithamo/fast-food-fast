f55df2b3763865ff10da83f06020bf426fcb8e8c
dmithamo <dennisbmithamo@gmail.com>
2018-10-11 07:15:22 +0300

    [finishes feature #161096028] : Store admin token on login, redirect to orders page

--- liveUI/fetchMenu.js
+++ liveUI/fetchMenu.js
@@ -8,12 +8,6 @@ const api_url = "https://dmithamo-fast-food-fast-api.herokuapp.com/api/v2";
 const menuUL = document.querySelector("#food-items");
 const menuContainer = document.querySelector("#menu-cont");
 
-// Login
-const loginBtn = document.querySelector("#login-btn");
-const loginEmailInput = document.querySelector("#login-email-input");
-const loginPasswordInput = document.querySelector("#login-password-input");
-
-
 // Helper function
 const appendToparent = (element, parent) => {
     parent.appendChild(element);
@@ -89,35 +83,8 @@ function fetchMenu() {
         });
 }
 
-function loginAdmin() {
-    // Retrieve login credentials
-    let email = loginEmailInput.innerHTML;
-    let password = loginPasswordInput.innerHTML;
-    console.log(email, password);
-
-
-    // fetch(`${api_url}/login`, {
-
-    // })
-    // .then((response) => response.json())
-    // .then(function(responseJSON) {
-    //     console.log(responseJSON);
-
-    // })
-    // .catch(function(err) {
-    //     console.log(err);
-    // })
-}
 
 // Call function when DOM content is finished loading
 document.addEventListener('DOMContentLoaded', () => {
     fetchMenu();
 });
-
-// Call function when login btn is clicked
-loginBtn.addEventListener('click', () => {
-    loginAdmin();
-});
-
-
-
