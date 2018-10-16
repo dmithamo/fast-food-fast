ef567c143a9f74d2ad827f1ad1739ccfa7f28288
dmithamo <dennisbmithamo@gmail.com>
2018-10-16 08:10:13 +0300

    [chore #] : Attempt to migrate to using flask instead of php

---
+++ run.py
@@ -0,0 +1,20 @@
+"""
+    Serve entry page of app using Flask
+"""
+
+from flask import Flask, url_for
+
+
+
+APP = Flask(__name__)
+
+@app.route('/')
+def serve_homepage():
+    """
+        Render homepage on visiting root url
+    """
+    return url_for("menu.html")
+
+
+if __name__ == '__main__':
+    APP.run(debug=True)
