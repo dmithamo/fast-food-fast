d476f6fd44051318bbd793b9c152a633797508d3
dmithamo <dennisbmithamo@gmail.com>
2018-10-16 08:42:37 +0300

    [chore #] : Store all current changes before checkout

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
     Serve entry page of app using Flask
 """
 
-from flask import Flask, url_for
+from flask import Flask, render_template
 
 
 
@@ -13,7 +13,7 @@ def serve_homepage():
     """
         Render homepage on visiting root url
     """
-    return url_for("menu.html")
+    return render_template("menu.html")
 
 
 if __name__ == '__main__':
